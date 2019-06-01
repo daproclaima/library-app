3441be2685c88558d78c2dc7a0ddd979e66e5fc7
Sébastien NOBOUR <sn.itpro@gmail.com>
2019-06-01 03:04:04 +0200

    added the validation but not working yet

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import {computed, observer} from '@ember/object';
-import {match, not, gte, and} from '@ember/object/computed';
+import {match, not, gte, and, empty} from '@ember/object/computed';
 
 
 export default Controller.extend({
@@ -8,10 +8,13 @@ export default Controller.extend({
     emailAddress: '',
     message: '',
     responseMessage: '',
-    emailAddressMatch: match('emailAddress', /^.+@.+\..+$/),
-    isLongEnough: gte('message.length', '5'),
-    isValid:  and('isLongEnough', 'emailAddressMatch'),
+
+
+    isValid:  and('isLongEnough', 'isEmailAddressMatch'),
     isDisabled: not('isValid'),
+
+    isLongEnough:'',
+    isEmailAddressMatch:'',
 
 
     actualEmailAddress: computed('emailAddress', function () {
@@ -31,18 +34,49 @@ export default Controller.extend({
 
     messageChanged: observer('message', function () {
         console.log('observer is called', this.get('message'));
+        this.actions.emailAddressMatch();
         // console.log('long enough ', this.get('isLongEnough'));
+        // console.log('isValid status ', this.get('isValid'));
+    }),
+
+    actualEmailAddressMatch: computed('isEmailAddressMatch', function () {
+        console.log('actualEmailAddressMatch function is called: ', this.get('isEmailAddressMatch'));
+    }),
+
+    emailAddressMatchChanged: observer('isEmailAddressMatch', function () {
+        console.log('observer is called', this.get('isEmailAddressMatch'));
+        this.actions.emailAddressMatch();
+        // console.log('mail matches ', this.get('emailAddressMatch'));
+        // console.log('isValid status ', this.get('isValid'));
+    }),
+
+    actualIsLongEnough: computed('isLongEnough', function () {
+        console.log('actualIsLongEnough function is called: ', this.get('isLongEnough'));
+        actions.longEnough();
+
+    }),
+
+    isLongEnoughChanged: observer('isLongEnough', function () {
+        console.log('observer is called', this.get('isLongEnough'));
+
+        // console.log('mail matches ', this.get('emailAddressMatch'));
         // console.log('isValid status ', this.get('isValid'));
     }),
 
 
     actions: {
 
+        emailAddressMatch(){
+                this.set('isEmailAddressMatch', match('emailAddress', /^.+@.+\..+$/));
+        },
+        longEnough(){
+                this.set('isLongEnough', gte('message.length', 5));
+        },
         sendMessage() {
             alert(`Saving of the following message is in progress...`);
             this.set('responseMessage', `Thank you! Here is the message. From ${this.get('emailAddress')}; Content: ${this.get('message')}`);
             this.set('message', ``);
-            this.set('emailAddress', '');
+            this.set('emailAddress', ``);
             alert(`We got your message and we’ll get in touch soon`);
         },
     },
